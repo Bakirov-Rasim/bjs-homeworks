64e19d434546af6ae938820d67c9eb2f607b4d44
Bakirov-Rasim <Bakir08@yandex.ru>
2019-11-06 11:48:05 +0300

    first homework

---
+++ variables/main.js
@@ -0,0 +1,29 @@
+// Задание 1
+console.log('Задание 1');
+let algebra;
+let geography;
+let physics;
+
+algebra = 5;
+geography = 4;
+physics = 3;
+
+average = (algebra + geography + physics) / 3;
+console.log(average);
+console.log();
+
+//Задание 2
+console.log('Задание 2');
+let myName = 'Rasim';
+let message = `Привет, мир! Меня зовут ${myName}`;
+console.log(message);
+console.log();
+
+//Задание 3
+console.log('Задание 3');
+let x = 2;
+let y = 22;
+let z = 0;
+
+result = x * y + 5 * z + x - 1;
+console.log(result);
